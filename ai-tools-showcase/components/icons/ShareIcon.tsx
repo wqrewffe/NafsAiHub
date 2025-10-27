936d08555ce7934b6815ab842deb3367c007ef01
wqrewffe <nexcodelababdullah@gmail.com>
2025-10-27 19:54:57 +0600

    Add files via upload

---
+++ ai-tools-showcase/components/icons/ShareIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const ShareIcon: React.FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+    aria-hidden="true"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4 12v8a2 2 0 002 2h12a2 2 0 002-2v-8m-4-6l-4-4m0 0L8 6m4-4v12"
+    />
+  </svg>
+);
