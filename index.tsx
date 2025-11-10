3459a9fdcc1eb91375e85cb6db6f8bfb10c29709
wqrewffe <nexcodelababdullah@gmail.com>
2025-11-10 10:38:32 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Preload critical resources
-const preloadLinks = [
-  { rel: 'modulepreload', href: '/App.tsx' },
-  { rel: 'modulepreload', href: '/components/Layout.tsx' },
-  { rel: 'modulepreload', href: '/hooks/useAuth.tsx' }
-];
-
-preloadLinks.forEach(link => {
-  const linkElement = document.createElement('link');
-  Object.entries(link).forEach(([key, value]) => linkElement.setAttribute(key, value));
-  document.head.appendChild(linkElement);
-});
-
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element to mount to");
 
